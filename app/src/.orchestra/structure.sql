83cae5d9b907b14bc6c9bd895f4fc70c73492320
Alannah Kearney <hi@alannahkearney.com>
2020-11-04 11:00:47 +1000

    chore(database): Clean and modernize structure.sql

--- app/src/.orchestra/structure.sql
+++ app/src/.orchestra/structure.sql
@@ -1,6 +1,5 @@
 # ************************************************************
 # Orchestra - Core Database Table Structure
-# Generation Time: 2020-03-29 20:16:53 +1000
 # ************************************************************
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -11,55 +10,35 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-# Dump of table tbl_authentication_error_log
-# ------------------------------------------------------------
-
-CREATE TABLE `tbl_authentication_error_log` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `message` varchar(255) DEFAULT NULL,
-  `payload` longtext DEFAULT NULL,
-  `datestamp` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-# Dump of table tbl_authors
-# ------------------------------------------------------------
-
 CREATE TABLE `tbl_authors` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `password` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
-  `first_name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `last_name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `last_seen` datetime DEFAULT '0000-00-00 00:00:00',
-  `user_type` enum('author','manager','developer') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'author',
-  `primary` enum('yes','no') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'no',
-  `default_area` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `auth_token_active` enum('yes','no') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'no',
-  `language` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username` varchar(20) NOT NULL DEFAULT '',
+  `password` varchar(150) NOT NULL,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `last_seen` datetime DEFAULT NULL,
+  `user_type` enum('author','manager','developer') NOT NULL DEFAULT 'author',
+  `primary` enum('yes','no') NOT NULL DEFAULT 'no',
+  `default_area` varchar(255) DEFAULT NULL,
+  `auth_token_active` enum('yes','no') NOT NULL DEFAULT 'no',
+  `language` varchar(15) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-# Dump of table tbl_cache
-# ------------------------------------------------------------
-
 CREATE TABLE `tbl_cache` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `hash` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `hash` varchar(32) NOT NULL DEFAULT '',
   `creation` int(14) NOT NULL DEFAULT 0,
   `expiry` int(14) unsigned DEFAULT NULL,
-  `data` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `namespace` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `data` longtext NOT NULL,
+  `namespace` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `hash` (`hash`),
   KEY `expiry` (`expiry`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-# Dump of table tbl_entries
-# ------------------------------------------------------------
 
 CREATE TABLE `tbl_entries` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -79,125 +58,84 @@ CREATE TABLE `tbl_entries` (
   KEY `modification_date_gmt` (`modification_date_gmt`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-# Dump of table tbl_extensions
-# ------------------------------------------------------------
-
 CREATE TABLE `tbl_extensions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `status` enum('enabled','disabled') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'enabled',
-  `version` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `status` enum('enabled','disabled') NOT NULL DEFAULT 'enabled',
+  `version` varchar(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-LOCK TABLES `tbl_extensions` WRITE;
-/*!40000 ALTER TABLE `tbl_extensions` DISABLE KEYS */;
-INSERT INTO `tbl_extensions` VALUES (NULL,'orchestra','enabled','1.0.0');
-INSERT INTO `tbl_extensions` VALUES (NULL,'console','enabled','1.1.1');
-/*!40000 ALTER TABLE `tbl_extensions` ENABLE KEYS */;
-UNLOCK TABLES;
-
-# Dump of table tbl_extensions_delegates
-# ------------------------------------------------------------
-
 CREATE TABLE `tbl_extensions_delegates` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `extension_id` int(11) NOT NULL,
-  `page` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `delegate` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `callback` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `page` varchar(100) NOT NULL,
+  `delegate` varchar(100) NOT NULL,
+  `callback` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `extension_id` (`extension_id`),
   KEY `page` (`page`),
   KEY `delegate` (`delegate`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-# Dump of table tbl_fields
-# ------------------------------------------------------------
-
 CREATE TABLE `tbl_fields` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `element_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `type` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `element_name` varchar(50) NOT NULL,
+  `type` varchar(32) NOT NULL,
   `parent_section` int(11) NOT NULL DEFAULT 0,
-  `required` enum('yes','no') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'yes',
+  `required` enum('yes','no') NOT NULL DEFAULT 'yes',
   `sortorder` int(11) NOT NULL DEFAULT 1,
-  `location` enum('main','sidebar') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'main',
-  `show_column` enum('yes','no') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'no',
+  `location` enum('main','sidebar') NOT NULL DEFAULT 'main',
+  `show_column` enum('yes','no') NOT NULL DEFAULT 'no',
   PRIMARY KEY (`id`),
   KEY `index` (`element_name`,`type`,`parent_section`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-# Dump of table tbl_fields_attachment
-# ------------------------------------------------------------
-
-CREATE TABLE `tbl_fields_attachment` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `field_id` int(11) unsigned NOT NULL,
-  `destination` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `validator` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `prepend_datestamp` enum('yes','no') NOT NULL default 'no',
-  PRIMARY KEY (`id`),
-  KEY `field_id` (`field_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-# Dump of table tbl_forgotpass
-# ------------------------------------------------------------
-
 CREATE TABLE `tbl_forgotpass` (
   `author_id` int(11) NOT NULL DEFAULT 0,
-  `token` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
-  `expiry` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `token` varchar(16) NOT NULL,
+  `expiry` varchar(25) NOT NULL,
   PRIMARY KEY (`author_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-# Dump of table tbl_pages
-# ------------------------------------------------------------
 
 CREATE TABLE `tbl_pages` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `parent` int(11) DEFAULT NULL,
-  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `handle` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `path` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `params` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `data_sources` text COLLATE utf8_unicode_ci DEFAULT NULL,
-  `events` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `handle` varchar(255) DEFAULT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `params` varchar(255) DEFAULT NULL,
+  `data_sources` text DEFAULT NULL,
+  `events` text DEFAULT NULL,
   `sortorder` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `parent` (`parent`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-# Dump of table tbl_pages_types
-# ------------------------------------------------------------
-
 CREATE TABLE `tbl_pages_types` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `page_id` int(11) unsigned NOT NULL,
-  `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `page_id` (`page_id`,`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-# Dump of table tbl_sections
-# ------------------------------------------------------------
-
 CREATE TABLE `tbl_sections` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `handle` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `handle` varchar(255) NOT NULL,
   `sortorder` int(11) NOT NULL DEFAULT 0,
-  `hidden` enum('yes','no') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'no',
-  `filter` enum('yes','no') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'yes',
-  `navigation_group` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Content',
+  `hidden` enum('yes','no') NOT NULL DEFAULT 'no',
+  `filter` enum('yes','no') NOT NULL DEFAULT 'yes',
+  `navigation_group` varchar(255) NOT NULL DEFAULT 'Content',
   `author_id` int(11) unsigned NOT NULL DEFAULT 1,
   `modification_author_id` int(11) unsigned NOT NULL DEFAULT 1,
-  `creation_date` datetime NOT NULL DEFAULT '2017-09-24 13:19:39',
-  `creation_date_gmt` datetime NOT NULL DEFAULT '2017-09-24 03:19:39',
-  `modification_date` datetime NOT NULL DEFAULT '2017-09-24 13:19:39',
-  `modification_date_gmt` datetime NOT NULL DEFAULT '2017-09-24 03:19:39',
+  `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `creation_date_gmt` datetime NOT NULL DEFAULT UTC_TIMESTAMP,
+  `modification_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modification_date_gmt` datetime NOT NULL DEFAULT UTC_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `handle` (`handle`),
   KEY `creation_date` (`creation_date`),
@@ -206,32 +144,33 @@ CREATE TABLE `tbl_sections` (
   KEY `modification_date_gmt` (`modification_date_gmt`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-# Dump of table tbl_sections_association
-# ------------------------------------------------------------
-
 CREATE TABLE `tbl_sections_association` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `parent_section_id` int(11) unsigned NOT NULL,
   `parent_section_field_id` int(11) unsigned DEFAULT NULL,
   `child_section_id` int(11) unsigned NOT NULL,
   `child_section_field_id` int(11) unsigned NOT NULL,
-  `hide_association` enum('yes','no') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'no',
-  `interface` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `editor` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `hide_association` enum('yes','no') NOT NULL DEFAULT 'no',
+  `interface` varchar(100) DEFAULT NULL,
+  `editor` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `parent_section_id` (`parent_section_id`,`child_section_id`,`child_section_field_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-# Dump of table tbl_sessions
-# ------------------------------------------------------------
-
 CREATE TABLE `tbl_sessions` (
-  `session` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `session` varchar(100) NOT NULL,
   `session_expires` int(10) unsigned NOT NULL DEFAULT 0,
-  `session_data` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `session_data` text DEFAULT NULL,
   PRIMARY KEY (`session`),
   KEY `session_expires` (`session_expires`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+LOCK TABLES `tbl_extensions` WRITE;
+/*!40000 ALTER TABLE `tbl_extensions` DISABLE KEYS */;
+INSERT INTO `tbl_extensions` VALUES (NULL,'orchestra','enabled','1.0.0');
+INSERT INTO `tbl_extensions` VALUES (NULL,'console','enabled','1.1.1');
+/*!40000 ALTER TABLE `tbl_extensions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
