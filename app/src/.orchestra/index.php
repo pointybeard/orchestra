1aa89f78f00b49a4b5ef83c32d0becd6b4e7a588
Alannah Kearney <hi@alannahkearney.com>
2020-11-08 12:43:33 +1000

    chore: Add attribution and tidy code

--- app/src/.orchestra/index.php
+++ app/src/.orchestra/index.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the "Orchestra" repository.
+ *
+ * Copyright 2019-2020 Alannah Kearney <hi@alannahkearney.com>
+ *
+ * For the full copyright and license information, please view the LICENCE
+ * file that was distributed with this source code.
+ */
+
 /**
  * index.php was removed from the essentials branch of pointybeard/symphonycms
  * so we need to add it back in. This file is symlink'd into the /www/*
@@ -9,16 +18,16 @@ declare(strict_types=1);
  */
 
 // Find out where we are:
-define('DOCROOT', realpath(__DIR__ . "/../lib/symphony"));
+define('DOCROOT', realpath(__DIR__.'/../lib/symphony'));
 
 // Propagate this change to all executables:
 chdir(DOCROOT);
 
 // Include autoloader:
-require_once DOCROOT . '/vendor/autoload.php';
+require_once DOCROOT.'/vendor/autoload.php';
 
 // Include the boot script:
-require_once DOCROOT . '/symphony/lib/boot/bundle.php';
+require_once DOCROOT.'/symphony/lib/boot/bundle.php';
 
 // Begin Symphony proper:
 symphony(
